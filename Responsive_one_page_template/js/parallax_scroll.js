d19abd332d5f5e067f26c68f49209088c809a3a9
Unknown <oxelika@gmail.com>
2019-04-07 01:11:47 +0300

    Add parallax section

--- Responsive_one_page_template/js/parallax_scroll.js
+++ Responsive_one_page_template/js/parallax_scroll.js
@@ -1,4 +1,9 @@
-$('.parallax-window').parallax({
+$('.parallax-header').parallax({
     imageSrc: 'img/bg-1.jpg',
     speed: 0.2
+});
+$('.parallax-services').parallax({
+    imageSrc: 'img/bg-2.jpg',
+    speed: 0.2,
+    bleed: -43.5
 });
